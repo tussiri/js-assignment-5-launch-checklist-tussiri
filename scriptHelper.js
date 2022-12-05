4247c441243ff39e4c0a21b430c252b9b2c26af9
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2022-12-04 22:06:03 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -45,10 +45,10 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         alert("input for the copilot cannot be a number.");
     } else if (validateInput(fuelLevel) === "Not a Number") {
         alert("input for the fuel level must be a number.");
-        // }
-        // else if (validateInput(cargoMass) === "Not a Number") {
-        //     alert("input for the cargo mass must be a number.");
-        // } 
+        }
+        else if (validateInput(cargoMass) === "Not a Number") {
+            alert("input for the cargo mass must be a number.");
+        } 
     } else {
         list.style.visibility = "visible";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
@@ -73,9 +73,9 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else {
-            list.style.visibility = "visible";
-            fuel.innerHTML = "Fuel level high enough for launch";
-            cargoStatus.innerHTML = 'Cargo mass low enough for launch';
+            list.style.visibility = "hidden";
+            fuel.innerHTML = " launch";
+            cargoStatus.innerHTML = 'launch';
             launchStatus.innerHTML = 'Shuttle is Ready for Launch';
             launchStatus.style.color = "#419F6A";
         }
