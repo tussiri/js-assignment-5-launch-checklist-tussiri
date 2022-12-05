b43f11929a5ed302407ac577d7b58bcbd891c451
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2022-12-04 21:30:23 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -53,22 +53,26 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         let launchStatus = document.getElementById("launchStatus");
         if (fuelLevel < 10000 && cargoLevel <= 10000) {
-faultyItems.style.visibility = 'visible';            fuel.innerHTML = 'Fuel level too low for launch';
+      
+            fuel.innerHTML = 'Fuel level too low for launch';
             cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
-faultyItems.style.visibility = 'visible';            fuel.innerHTML = 'Fuel level high enough for launch';
+    
+            fuel.innerHTML = 'Fuel level high enough for launch';
             cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else if (fuelLevel < 10000 && cargoLevel > 10000) {
-faultyItems.style.visibility = 'visible';            fuel.innerHTML = 'Fuel level too low for launch';
+        
+            fuel.innerHTML = 'Fuel level too low for launch';
             cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else {
-faultyItems.style.visibility = 'visible';            fuel.innerHTML = 'Fuel level high enough for launch';
+         
+            fuel.innerHTML = 'Fuel level high enough for launch';
             cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.innerHTML = 'Shuttle is Ready for Launch';
             launchStatus.style.color = "#419F6A";
