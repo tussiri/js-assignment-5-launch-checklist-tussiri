5042be4bca8b78f92114d19d710159d55f9c421a
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2022-12-04 22:40:13 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -81,7 +81,10 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-            return response.json();
+         if (response.status >= 400) {
+            throw new Error('Bad response');
+        } else {    
+        return response.json();
         }
 
     });
