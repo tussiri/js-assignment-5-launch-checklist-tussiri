9f309b58d4043a5e1de73dd3687d97aab3f7f900
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2022-12-04 22:53:39 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -32,19 +32,19 @@ function validateInput(testInput) {
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let pilotStatus = document.getElementById("pilotStatus");
-    let fuel = document.getElementById("fuelStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
     let copilotStatus = document.getElementById('copilotStatus');
     let cargoStatus = document.getElementById('cargoStatus');
 
     if (validateInput(pilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(copilot) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert(`All fields are required!`);
         list.style.visibility = "hidden";
-    } else if (validateInput(pilot) === "Is a Number") {
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
         alert("input for the pilot cannot be a number.");
-    } else if (validateInput(copilot) === "Is a Number") {
-        alert("input for the copilot cannot be a number.");
-    } else if (validateInput(fuelLevel) === "Not a Number") {
-        alert("input for the fuel level must be a number.");
+//     } else if (validateInput(copilot) === "Is a Number") {
+//         alert("input for the copilot cannot be a number.");
+//     } else if (validateInput(fuelLevel) === "Not a Number") {
+//         alert("input for the fuel level must be a number.");
         // }
         // else if (validateInput(cargoMass) === "Not a Number") {
         //     alert("input for the cargo mass must be a number.");
@@ -55,31 +55,26 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         let launchStatus = document.getElementById("launchStatus");
         if (fuelLevel < 10000 && cargoLevel <= 10000) {
-            list.style.visibility = "visible";
-            fuel.innerHTML = 'Fuel level too low for launch';
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
             cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
-            list.style.visibility = "visible";
-            fuel.innerHTML = 'Fuel level high enough for launch';
+            fuelStatus.innerHTML = 'Fuel level high enough for launch';
             cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else if (fuelLevel < 10000 && cargoLevel > 10000) {
-            list.style.visibility = "visible";
-            fuel.innerHTML = 'Fuel level too low for launch';
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
             cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
             launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
             launchStatus.style.color = "#c7254e";
         } else {
-            list.style.visibility = "visible";
-            fuel.innerHTML = "Fuel level high enough for launch";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
             cargoStatus.innerHTML = 'Cargo mass low enough for launch';
             launchStatus.innerHTML = 'Shuttle is Ready for Launch';
             launchStatus.style.color = "#419F6A";
         }
-
     }
 }
 
@@ -87,14 +82,8 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-        if (response.status >= 400) {
-            throw new Error('Bad response');
-        } else {
             return response.json();
-        }
-
     });
-
     return planetsReturned;
 }
 
@@ -107,5 +96,4 @@ module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet;
-
 module.exports.myFetch = myFetch;
